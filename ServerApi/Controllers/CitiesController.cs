d935a2844f35417f67d6666a6678dbb6ab30e6e4
virshu <felix@rabinovich.org>
2024-03-14 23:14:19 -0700

    Created CitieController and enabled DbContext

---
+++ ServerApi/Controllers/CitiesController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorldCitiesModel;
+
+namespace ServerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController(WorldCitiesContext context) : ControllerBase
+    {
+        // GET: api/Cities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        {
+            return await context.Cities.ToListAsync();
+        }
+
+        // GET: api/Cities/5
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<City>> GetCity(int id)
+        {
+            City? city = await context.Cities.FindAsync(id);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return city;
+        }
+
+        // PUT: api/Cities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> PutCity(int id, City city)
+        {
+            if (id != city.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(city).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<City>> PostCity(City city)
+        {
+            context.Cities.Add(city);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCity", new { id = city.Id }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            City? city = await context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            context.Cities.Remove(city);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CityExists(int id)
+        {
+            return context.Cities.Any(e => e.Id == id);
+        }
+    }
+}
--- ServerApi/Program.cs
+++ ServerApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WorldCitiesModel;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WorldCitiesContext>(optionsBuilder =>
+
+    optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 WebApplication app = builder.Build();
 
