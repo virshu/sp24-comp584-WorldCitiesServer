e0675fd7751717aaa6024444c1fe3059acf98ad3
virshu <felix@rabinovich.org>
2024-04-07 13:30:04 -0700

    Setup authentication to get 401

---
+++ ServerApi/JwtHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using WorldCitiesModel;
+
+internal class JwtHandler(IConfiguration configuration, UserManager<WorldCitiesUser> userManager)
+{
+    public async Task<JwtSecurityToken> GetTokenAsync(WorldCitiesUser user) =>
+        new(
+            issuer: configuration["JwtSettings:Issuer"],
+            audience: configuration["JwtSettings:Audience"],
+            claims: await GetClaimsAsync(user),
+            expires: DateTime.Now.AddMinutes(Convert.ToDouble(configuration["JwtSettings:ExpirationTimeInMinutes"])),
+            signingCredentials: GetSigningCredentials());
+
+    private SigningCredentials GetSigningCredentials() {
+        byte[] key = Encoding.UTF8.GetBytes(configuration["JwtSettings:SecurityKey"]!);
+        SymmetricSecurityKey secret = new(key);
+        return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+    }
+
+    private async Task<List<Claim>> GetClaimsAsync(WorldCitiesUser user) {
+        List<Claim> claims = [new Claim(ClaimTypes.Name, user.UserName!)];
+        claims.AddRange(from role in await userManager.GetRolesAsync(user) select new Claim(ClaimTypes.Role, role));
+        return claims;
+    }
+
+}
--- ServerApi/Program.cs
+++ ServerApi/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using WorldCitiesModel;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -11,8 +14,33 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<WorldCitiesContext>(optionsBuilder =>
-
     optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<WorldCitiesUser, IdentityRole>()
+    .AddEntityFrameworkStores<WorldCitiesContext>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        RequireExpirationTime = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+            builder.Configuration["JwtSettings:SecurityKey"] ?? throw new InvalidOperationException()))
+    };
+});
+builder.Services.AddScoped<JwtHandler>();
 
 WebApplication app = builder.Build();
 
@@ -27,6 +55,7 @@ app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
